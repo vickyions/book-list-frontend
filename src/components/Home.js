b884f1231eae9ecc474d1e85b043dd9fe4fa9c21
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-12-03 16:50:16 +0530

    completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import "./Home.css";
-import {Outlet} from "react-router-dom"
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const logOut = (e) => {
+    localStorage.removeItem("book-token");
+    navigate("/");
+  };
+
+  useEffect(() => {
+    if (
+      !localStorage.getItem("book-token") &&
+      location.pathname.split("/")[1] === "books"
+    ) {
+      return navigate("/");
+    }
+  }, [navigate, location.pathname]);
   return (
-    <div className='home-wrapper'>
-        <Outlet/>
+    <div className="home-wrapper">
+      {location.pathname.split("/")[1] === "books" && (
+        <button id="logout" type="button" onClick={logOut}>
+          Logout
+        </button>
+      )}
+      <Outlet />
     </div>
-  )
+  );
 }
